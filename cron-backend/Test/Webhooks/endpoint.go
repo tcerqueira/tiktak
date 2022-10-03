4ed3480406f6afe9add650931a85e138d48302f9
tcerqueira <tiago.cerqueira7777@gmail.com>
2022-10-03 19:16:12 +0100

    Test server to test webhook calls

---
+++ cron-backend/Test/Webhooks/endpoint.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+var (
+	sv_port string
+)
+
+func init() {
+	if len(os.Args) > 1 {
+		sv_port = os.Args[1]
+	} else {
+		sv_port = "8050"
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	credentials := handlers.AllowCredentials()
+	methods := handlers.AllowedMethods([]string{"*"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	// Log requests
+	router.HandleFunc("/webhook", handleGet).Methods("GET")
+	router.HandleFunc("/webhook", handleOthers).Methods("POST", "PUT", "PATCH", "DELETE")
+
+	// Start server
+	fmt.Println("Starting server...")
+	log.Fatal(http.ListenAndServe(":"+sv_port, handlers.CORS(credentials, methods, origins)(router)))
+}
+
+type RequestBody struct {
+	Body string
+}
+
+func handleGet(res http.ResponseWriter, req *http.Request) {
+	body := req.URL.Query().Get("body")
+	handle(res, req, body)
+}
+
+func handleOthers(res http.ResponseWriter, req *http.Request) {
+	var body RequestBody
+	json.NewDecoder(req.Body).Decode(&body)
+	handle(res, req, body.Body)
+}
+
+func handle(res http.ResponseWriter, req *http.Request, body string) {
+	fmt.Printf("%s - %s\n", time.Now().Format("2006-02-01 15:04:05"), body)
+	res.Write([]byte(""))
+}
